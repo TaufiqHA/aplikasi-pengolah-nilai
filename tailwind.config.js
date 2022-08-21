afade4776224b8b13bc225d39cd242b231e5104c
TaufiqHA <htaufiq225@gmail.com>
2022-08-21 22:46:46 +0700

    konfigurasi tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:
+      {
+        'primary': '#14b8a6',
+      }
+    },
   },
   plugins: [],
 }
